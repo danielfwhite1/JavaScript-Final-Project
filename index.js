3e7350010080163f3fca43ce70c6c08f56fb5ab0
Daniel White <danielfwhite1@gmail.com>
2020-07-21 00:15:06 -0700

    completed final round button and properly displayed final scores

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ let player2Hand = [];
 let roundIndex = 0;
 let player1Score = 0;
 let player2Score = 0;
-let winner = '';
+let roundWinner = '';
+let gameWinner = '';
 
 
 const deck1 = new Deck();
@@ -81,52 +82,117 @@ function playGame() {
             console.log("Player 1 Wins!");
             player1Score += 1;       
             console.log(`Player 1 New Score: ${player1Score}`)
-            winner = 'Player 1';
-            // document.getElementById("winner").innerHTML = `Player 1 Wins!`;
+            roundWinner = 'Player 1';
         } else if (player2Hand[roundIndex].rank > player1Hand[roundIndex].rank) {     
             console.log("Player 2 Wins!");
             player2Score += 1;
             console.log(`Player 2 New Score: ${player2Score}`)
-            winner = 'Player 2';
-            // document.getElementById("p1score").innerHTML = `Player 2 Wins!`;
+            roundWinner = 'Player 2';
         } else {
             console.log("It's a Tie!");
-            // document.getElementById("winner").innerHTML = "It's a tie!";
-            winner = "It's a tie";
+            roundWinner = "It's a tie";
 
         }
 
-    // console.log(`Final Score:
+        // if (roundIndex == 25) {
 
-    // Player 1: ${player1Score}
-    // Player 2: ${player2Score}
-    
-    // GAME OVER`)
+        //     document.getElementById("nextRoundButton").addEventListener("click", () => {
+                
+        //         document.getElementById("game").style = "display: none;";
+        //         document.getElementById("final").style = "display: block;";
+        //         document.getElementById("p1Score").innerHTML = `Score: ${player1Score}`;
+        //         document.getElementById("p2Score").innerHTML = `Score: ${player2Score}`;
+        //         document.getElementById("gameWinner").innerHTML = `${gameWinner} Wins!`;
+        //         document.getElementById("final").style = "display: none;";
+     
+        //     });
+        // }
+
+        if (roundIndex == 24) {
+
+            document.getElementById("nextRoundButton").addEventListener("click", () => {
+                
+                document.getElementById("game").style = "display: none;";
+                document.getElementById("finalRound").style = "display: block;";
+
+                document.getElementById("lastRound").innerHTML = `Round ${roundIndex}`;
+                document.getElementById("p1FinalScore").innerHTML = `Score: ${player1Score}`;
+                document.getElementById("p2FinalScore").innerHTML = `Score: ${player2Score}`;
+                document.getElementById("p1FinalHand").innerHTML = `${player1Hand[roundIndex - 1].value} of ${player1Hand[roundIndex - 1].suit}`;
+                document.getElementById("p2FinalHand").innerHTML = `${player2Hand[roundIndex - 1].value} of ${player2Hand[roundIndex - 1].suit}`;
+                // playGame(deck1);
+                document.getElementById("finalRoundWinner").innerHTML = `Round ${roundIndex} Winner: ${roundWinner}`;
+     
+            });
+        }
+
+        if (roundIndex == 25) {
+
+            if (player1Score > player2Score) {
+                gameWinner = 'Player 1';
+            } else if (player2Score > player1Score) {
+                gameWinner = 'Player 2';
+            } else {
+                gameWinner = 'No one';
+            }
+
+            document.getElementById("finalButton").addEventListener("click", () => {
+                
+                document.getElementById("game").style = "display: none;";
+                document.getElementById("finalRound").style = "display: none;";
+                document.getElementById("final").style = "display: block;";
+                document.getElementById("p1TotalScore").innerHTML = `Player 1 Score: ${player1Score}`;
+                document.getElementById("p2TotalScore").innerHTML = `Player 2 Score: ${player2Score}`;
+                document.getElementById("gameWinner").innerHTML = `${gameWinner} Wins!`;
+     
+            });
+
+            console.log(`Final Score:
+
+            Player 1: ${player1Score}
+            Player 2: ${player2Score}
+
+            ${gameWinner} Wins!
+        
+            GAME OVER`);
+        }
 
     roundIndex++;
 }
 
-
-
 document.getElementById("nextRoundButton").addEventListener("click", () => {
+
+    // if (roundIndex == 25) {
+    //     document.getElementById("finalButton").addEventListener("click", () => {
+    //         document.getElementById("game").style = "display: none;";
+    //         document.getElementById("final").style = "display: block;";
+    //         document.getElementById("p1Score").innerHTML = `Score: ${player1Score}`;
+    //         document.getElementById("p2Score").innerHTML = `Score: ${player2Score}`;
+    //         document.getElementById("gameWinner").innerHTML = `${gameWinner} Wins!!!`;
+    //         document.getElementById("finalButton").style = "display: none;";
+    //     })
+    // }
+
     document.getElementById("round").innerHTML = `Round ${roundIndex + 1}`;
-    document.getElementById("p1score").innerHTML = `Score: ${player1Score}`;
-    document.getElementById("p2score").innerHTML = `Score: ${player2Score}`;
-    document.getElementById("p1hand").innerHTML = `${player1Hand[roundIndex].value} of ${player1Hand[roundIndex].suit}`;
-    document.getElementById("p2hand").innerHTML = `${player2Hand[roundIndex].value} of ${player2Hand[roundIndex].suit}`;
+    document.getElementById("p1Score").innerHTML = `Score: ${player1Score}`;
+    document.getElementById("p2Score").innerHTML = `Score: ${player2Score}`;
+    document.getElementById("p1Hand").innerHTML = `${player1Hand[roundIndex].value} of ${player1Hand[roundIndex].suit}`;
+    document.getElementById("p2Hand").innerHTML = `${player2Hand[roundIndex].value} of ${player2Hand[roundIndex].suit}`;
     playGame(deck1);
-    document.getElementById("winner").innerHTML = `Round ${roundIndex} Winner: ${winner}`;
+    document.getElementById("roundWinner").innerHTML = `Round ${roundIndex} Winner: ${roundWinner}`
+    
 })
+
+
 
 document.getElementById("startButton").addEventListener("click", () => {
     document.getElementById("welcome").style = "display: none;";
     document.getElementById("game").style = "display: block";
     document.getElementById("round").innerHTML = `Round ${roundIndex + 1}`;
-    document.getElementById("p1score").innerHTML = `Score: ${player1Score}`;
-    document.getElementById("p2score").innerHTML = `Score: ${player2Score}`;
-    document.getElementById("p1hand").innerHTML = `${player1Hand[roundIndex].value} of ${player1Hand[roundIndex].suit}`;
-    document.getElementById("p2hand").innerHTML = `${player2Hand[roundIndex].value} of ${player2Hand[roundIndex].suit}`;
+    document.getElementById("p1Score").innerHTML = `Score: ${player1Score}`;
+    document.getElementById("p2Score").innerHTML = `Score: ${player2Score}`;
+    document.getElementById("p1Hand").innerHTML = `${player1Hand[roundIndex].value} of ${player1Hand[roundIndex].suit}`;
+    document.getElementById("p2Hand").innerHTML = `${player2Hand[roundIndex].value} of ${player2Hand[roundIndex].suit}`;
     playGame(deck1);
-    document.getElementById("winner").innerHTML = `Round ${roundIndex} Winner: ${winner}`;
+    document.getElementById("roundWinner").innerHTML = `Round ${roundIndex} Winner: ${roundWinner}`;
 });
-
